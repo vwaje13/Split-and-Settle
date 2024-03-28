1fff47ff02b728ac70c443fe4d38d13cea91c358
Ahmed Elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2024-03-28 01:28:57 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,23 @@
+public class User {
+    int userID;
+    String name;
+    String email;
+    String phoneNumber;
+    String password;
+
+    void login(String attemptEmail, String attemptPassword){
+
+    }
+
+    void logout(){
+
+    }
+
+    void createAccount(String pName, String pEmail, String pPhoneNumber, String pPassword){
+
+    }
+
+    void resetPassword(String resetEmail){
+        
+    }
+}
