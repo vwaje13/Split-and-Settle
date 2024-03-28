ee50b54fca847331731409835f24725488a33ff5
Ahmed Elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2024-03-28 01:26:48 -0500

    Create Group.java

---
+++ Group.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Group{
+    int groupID;
+    String name;
+    List <User> members;
+
+    Group createGroup(String name, List<User> newMembers){ // returning group instead of void so it can be assigned to a user
+        return null;
+    }
+
+    void inviteMember(User invitedUser){
+
+    }
+
+    void removeMember(User removedUser){
+
+    }
+
+    void deleteGroup(){
+        
+    }
+}
