caefdcfd4cf3134decde64669e3ea8493f593f3d
Ahmed Elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2024-03-28 01:26:15 -0500

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,15 @@
+import java.sql.Date;
+
+public class Activity {
+    int activityID; // is this not redundant with the ohe beneath it?
+    int expenseID;
+    int groupID;
+    String type;
+    Date acitivityDate;
+    User activeUser;
+
+    void logActivity(User pUser, Date pDate, int pExpenseID, pGroupID) // p meaning "Parameter"
+    {
+        
+    }
+}
