7fa1277dbd8ea9d7288f2f402e971fb8119fb385
Ahmed Elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2024-03-28 01:26:28 -0500

    Create Debt.java

---
+++ Debt.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Debt {
+    int debtID;
+    double amountOwed;
+    User debtor;
+    User debtCollector;
+    int GroupID;
+
+    List<Debt> viewDebts(int UserID, int GroupID){
+        return null;
+    }
+}
