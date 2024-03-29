0b9c75dc6a53b3c9779548679e3228487fae5942
Dhriti Vivek <dhritivivek2003@gmail.com>
2024-03-29 18:37:46 -0500

    Activity part

--- Backend/Activity.java
+++ Backend/Activity.java
@@ -1,17 +1,11 @@
 import java.sql.Date;
-
-//Dhriti//
+// Dhriti 
 
 public class Activity {
-    int activityID; // is this not redundant with the ohe beneath it?
-    int expenseID;
-    int groupID;
-    String type;
-    Date acitivityDate;
-    User activeUser;
-
-    void logActivity(User pUser, Date pDate, int pExpenseID, pGroupID) // p meaning "Parameter"
-    {
-        
-    }
+  int activityID;
+  int expenseID;
+  int groupID;
+  String type;
+  Date activityDate; // records date for every activity that happens
+  User activeUser;
 }
