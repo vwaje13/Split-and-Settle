838c3b1efa68b665c481f3a7d1ea3c38c41c8d1b
Ayush Sheth <87743570+Eclipse10@users.noreply.github.com>
2024-03-29 11:12:56 -0500

    Delete main2.java

--- main2.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String [] args){
-        System.out.println("Get to working Ayush!!!"); 
-        System.out.println("Ayush, you haven't done anything bruh!!");
-    }
-}
