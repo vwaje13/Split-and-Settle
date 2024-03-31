96716478d7a383f50aec01aa57e6ff091ba65a14
Ayush Sheth <87743570+Eclipse10@users.noreply.github.com>
2024-03-31 18:39:01 -0500

    Update Group.java

--- Backend/Group.java
+++ Backend/Group.java
@@ -1,25 +1,80 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 //Ayush//
 
-public class Group{
-    int groupID;
-    String name;
-    List <User> members;
+public class Group {
+    private int groupID;
+    private String name;
+    private List<Integer> memberIDs;
 
-    Group createGroup(String name, List<User> newMembers){ // returning group instead of void so it can be assigned to a user
-        return null;
+    public Group(int groupID, String name) {
+        this.groupID = groupID;
+        this.name = name;
+        this.memberIDs = new ArrayList<Integer>();
     }
 
-    void inviteMember(User invitedUser){
-
+    public Group createGroup(String name, List<Integer> newMemberIDs) {
+        Group newGroup = new Group(generateGroupID(), name);
+        newGroup.addMembers(newMemberIDs);
+        return newGroup;
     }
 
-    void removeMember(User removedUser){
-
+    public void inviteMember(int invitedUserID) {
+        memberIDs.add(invitedUserID);
+        inviteUserToGroup(invitedUserID);
     }
 
-    void deleteGroup(){
-        
+    public void removeMember(int removedUserID) {
+        memberIDs.remove(Integer.valueOf(removedUserID));
+        removeUserFromGroup(removedUserID);
+    }
+
+    public void deleteGroup() {
+        deleteGroupFromSystem(this);
+    }
+
+    private void addMembers(List<Integer> newMemberIDs) {
+        memberIDs.addAll(newMemberIDs);
+        for (int userID : newMemberIDs) {
+            inviteUserToGroup(userID);
+        }
+    }
+
+    private int generateGroupID() {
+        // Generate a unique ID for the group using the UUID class
+        return UUID.randomUUID().hashCode();
+    }
+
+    private void inviteUserToGroup(int userID) {
+        // Add the user to the group's list of members
+        System.out.println("Inviting user with ID " + userID + " to join group " + name);
+    }
+
+    private void removeUserFromGroup(int userID) {
+        // Remove the user from the group's list of members
+        System.out.println("Removing user with ID " + userID + " from group " + name);
+    }
+
+    private void deleteGroupFromSystem(Group group) {
+        // Remove the group from the list of groups for all members
+        for (int userID : group.getMemberIDs()) {
+            removeUserFromGroup(userID);
+        }
+        // Remove the group from the system
+        System.out.println("Deleting group " + name);
+    }
+
+    public List<Integer> getMemberIDs() {
+        return memberIDs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGroupID() {
+        return groupID;
     }
 }
