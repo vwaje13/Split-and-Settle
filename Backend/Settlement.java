749c3224fed0ab82307fa191003809abab974422
huzaifa32 <syedhuzaifa.nabeel@gmail.com>
2024-03-31 19:22:46 -0500

    added settlement class

--- Backend/Settlement.java
+++ Backend/Settlement.java
@@ -1,1 +1,32 @@
 //Huzaifa//
+
+import java.util.Date;
+
+public class Settlement{
+    int transactionID, expenseID;
+    double amount;
+    Date date;
+    User payer, receiver;
+
+    static boolean settleStatus;
+
+
+    public void createTransaction(int expenseID,double amount,User payer, User receiver){
+        this.expenseID=expenseID;
+        this.amount=amount;
+        this.payer=payer;
+        this.receiver=receiver;
+        this.date = new Date();
+    }
+
+    public void settleTransaction(int transactionID){
+        this.transactionID=transactionID;
+    }
+
+    public static Boolean isSettled(int userID){
+        return settleStatus;
+    }
+
+}
+
+
