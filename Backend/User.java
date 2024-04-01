3efd0479845f8a1328da0e9a58ce9a9e8c8d4da2
datnguyentx12 <165223065+datnguyentx12@users.noreply.github.com>
2024-03-31 20:21:11 -0500

    Update User.java

--- Backend/User.java
+++ Backend/User.java
@@ -13,6 +13,7 @@ public class User {
 
     //Dat//
 
+    //The user will input their name, email, phone number, and password. userID is automatically done by the system and it is the size of the users list.
     public User(int userID, String name, String email, String phoneNumber, String password){
         this.userID = userID;
         this.name = name;
@@ -21,6 +22,7 @@ public class User {
         this.password = password;
     }
 
+    //Check through users list for a matching email and password combination. A success message is shown if there is a match and if there are no matches, then output an error message.
     void login(String attemptEmail, String attemptPassword){
         for (User user : users) {
             if (user.email.equals(attempEmail) && user.password.equals(attemptPassword)) {
@@ -30,10 +32,12 @@ public class User {
         System.out.println("Login failed. Email or password is incorrect.");
     }
 
+    //Print message to user saying that they logged out.
     void logout(){
         System.out.println(this.name + " has logged out.");
     }
 
+    //ID is increased sequentially as users are added, which is the size of the users list. Creates a new user with inputted data and adds the user to the list.
     void createAccount(String pName, String pEmail, String pPhoneNumber, String pPassword){
         ID = users.size() + 1;
         User user = new User(ID, pName, pEmail, pPhoneNumber, pPassword);
@@ -41,6 +45,7 @@ public class User {
         System.out.println("New account has been created for " + pEmail);
     }
 
+    //If the user email exists, then send instructions to reset password to the email. If user does not exist, then output error.
     void resetPassword(String resetEmail){
         for (User user: users) {
             if (user.email.equals(resetEmail)) {
